d4fa7a0bf01703a9a5c1e9e9c6759b7731b7a581
whotouchedmybible <trentjcronin@gmail.com>
2020-01-23 21:17:23 -0600

    testing webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,21 @@
 // For a more comprehensive configuration check:
 // https://github.com/fable-compiler/webpack-config-template
 
+// If we're running the webpack-dev-server, assume we're in development mode
+var isProduction = !process.argv.find(v => v.indexOf('webpack-dev-server') !== -1);
+console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
+
 var path = require("path");
 
 module.exports = {
     mode: "development",
     entry: "./src/App.fsproj",
     output: {
-        path: path.join(__dirname, "./public"),
+        path: path.join(__dirname, "./"),
         filename: "bundle.js",
     },
     devServer: {
-        contentBase: "./public",
+        contentBase: "./",
         port: 8080,
     },
     module: {
