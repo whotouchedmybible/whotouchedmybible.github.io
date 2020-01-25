41ffdd6e41379a904178e3b5490f16ed46e5f299
whotouchedmybible <trentjcronin@gmail.com>
2020-01-24 18:57:13 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -95,6 +95,7 @@ module.exports = {
     // to prevent browser caching if code changes
     output: {
         path: resolve(CONFIG.outputDir),
+		publicPath: './',
         filename: isProduction ? '[name].[hash].js' : '[name].js'
     },
     mode: isProduction ? 'production' : 'development',
